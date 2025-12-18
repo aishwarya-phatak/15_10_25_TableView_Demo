3d74c00b554da3fcb7ec9fe1f3aa19e08f2f6957
aishwarya <ashp17star@gmail.com>
2025-12-18 13:14:52 +0530

    .

--- 15_10_25_TableView_Demo/ViewController.swift
+++ 15_10_25_TableView_Demo/ViewController.swift
@@ -7,26 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+class ViewController: UIViewController{
 
     @IBOutlet weak var menuItemsTableView: UITableView!
+    
+    var vegMenuItems = ["Dosa", "Idli","Pulav","Samosa Chaat","Uttappa"]
 
+    var nonvegMenuItems = ["Chicken Biryani", "Abc","Abc","Abc","Abc"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         menuItemsTableView.delegate = self
         menuItemsTableView.dataSource = self
     }
     
+}
+
+//MARK : Menu Table View conforms to UITableViewDataSource
+extension ViewController : UITableViewDataSource{
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if section == 0 {
+            vegMenuItems.count
+        }else{
+            nonvegMenuItems.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let basicCell = self.menuItemsTableView.dequeueReusableCell(withIdentifier: "BasicCell", for: indexPath)
-        
-        basicCell.textLabel?.text = "Bitcode \(indexPath.section) -- \(indexPath.row)"
-        basicCell.backgroundColor = .orange
-        
-        return basicCell
+        if indexPath.section == 0{
+            
+            let basicCell = self.menuItemsTableView.dequeueReusableCell(withIdentifier: "BasicCell", for: indexPath)
+            
+            basicCell.textLabel?.text = "Bitcode \(indexPath.section) -- \(indexPath.row)"
+            basicCell.backgroundColor = .orange
+            
+            return basicCell
+        }else{
+            let basicCell = self.menuItemsTableView.dequeueReusableCell(withIdentifier: "BasicCell", for: indexPath)
+            
+            basicCell.textLabel?.text = "Bitcode \(indexPath.section) -- \(indexPath.row)"
+            basicCell.backgroundColor = .cyan
+            return basicCell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            return "Veg Menu Items"
+        }else{
+            return "Non Veg Menu Items"
+        }
+    }
+}
+
+
+//MARK : Menu Table View conforms to UITableViewDelegate
+extension ViewController : UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
     }
 }
