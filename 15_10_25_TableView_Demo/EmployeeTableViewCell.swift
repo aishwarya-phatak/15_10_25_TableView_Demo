48ab11d65af67b30b593b2f16e303a7e17a34087
aishwarya <ashp17star@gmail.com>
2025-12-19 13:20:29 +0530

    Employee Custom Cell

---
+++ 15_10_25_TableView_Demo/EmployeeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmployeeTableViewCell.swift
+//  15_10_25_TableView_Demo
+//
+//  Created by Vishal Jagtap on 19/12/25.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ 15_10_25_TableView_Demo/EmployeeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmployeeViewController.swift
+//  15_10_25_TableView_Demo
+//
+//  Created by Vishal Jagtap on 19/12/25.
+//
+
+import UIKit
+
+class EmployeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
