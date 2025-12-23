c2c83ab06ad2d7f98f8e8500adbe595ae2f473a1
aishwarya <ashp17star@gmail.com>
2025-12-23 13:28:09 +0530

    Table View with navigation controller

---
+++ 15_10_25_TableView_Demo/EmployeeDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmployeeDetailsViewController.swift
+//  15_10_25_TableView_Demo
+//
+//  Created by Vishal Jagtap on 23/12/25.
+//
+
+import UIKit
+
+class EmployeeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
